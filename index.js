d5017f969ac54ff32d897bba4a49eb4d6a51b730
sunfkny <1198355143@qq.com>
2021-02-09 05:22:06 +0800

    添加总次数	保底内; 添加 首行冻结; 适配 分析工具

--- index.js
+++ index.js
@@ -81,21 +81,35 @@
   console.log("开始获取抽卡记录");
   const workbook = new ExcelJS.Workbook();
   for (const type of gachaTypes) {
-    const sheet = workbook.addWorksheet(type.name);
+    const sheet = workbook.addWorksheet(type.name, {views: [{state: 'frozen', ySplit: 1}]});
     sheet.columns = [
       { header: "时间", key: "time", width: 24 },
-      { header: "名称", key: "name", width: 18 },
-      { header: "类型", key: "type", width: 6 },
-      { header: "星级", key: "rank", width: 6 },
+      { header: "名称", key: "name", width: 14 },
+      { header: "类别", key: "type", width: 8 },
+      { header: "星级", key: "rank", width: 8 },
+      { header: "总次数", key: "idx", width: 8 },
+      { header: "保底内", key: "pdx", width: 8 },
     ];
     // get gacha logs
     const logs = (await getGachaLogs(type.name, type.key)).map((item) => {
       // const match = data.find((v) => v.item_id === item.item_id);
-      return [item.time, item.name, item.item_type, item.rank_type];
+      return [item.time, item.name, item.item_type, parseInt(item.rank_type)];
     });
+    logs.reverse();
+    idx = 0;
+    pdx = 0
+    for (log of logs){
+      idx += 1;
+      pdx += 1;
+      log.push(idx,pdx);
+      if (log[3] === "5") {
+        pdx = 0;
+      }
+    }
+    // console.log(logs);
     sheet.addRows(logs);
     // set xlsx hearer style
-    ["A", "B", "C", "D"].forEach((v) => {
+    ["A", "B", "C", "D","E","F"].forEach((v) => {
       sheet.getCell(`${v}1`).border = {
         top: {style:'thin', color: {argb:'ffc4c2bf'}},
         left: {style:'thin', color: {argb:'ffc4c2bf'}},
@@ -116,7 +130,7 @@
     });
     // set xlsx cell style
     logs.forEach((v, i) => {
-      ["A", "B", "C", "D"].forEach((c) => {
+      ["A", "B", "C", "D","E","F"].forEach((c) => {
         sheet.getCell(`${c}${i + 2}`).border = {
           top: {style:'thin', color: {argb:'ffc4c2bf'}},
           left: {style:'thin', color: {argb:'ffc4c2bf'}},
