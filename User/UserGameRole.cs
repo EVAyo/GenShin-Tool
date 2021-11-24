6361f2e0b4ef8ba969ad046e24a59bde15474c87
DismissedLight <1686188646@qq.com>
2021-11-24 21:45:52 +0800

    Update UserGameRole.cs

--- User/UserGameRole.cs
+++ User/UserGameRole.cs
@@ -9,7 +9,7 @@ namespace DGP.Genshin.MiHoYoAPI.GameRole
         [JsonProperty("game_uid")] public string? GameUid { get; set; }
         [JsonProperty("nickname")] public string? Nickname { get; set; }
         [JsonProperty("level")] public int Level { get; set; }
-        [JsonProperty("is_chosen")] public string? IsChosen { get; set; }
+        [JsonProperty("is_chosen")] public bool IsChosen { get; set; }
         [JsonProperty("region_name")] public string? RegionName { get; set; }
         [JsonProperty("is_official")] public string? IsOfficial { get; set; }
 
