73a2faa14aa01d84a2922c14e69baeed719438a0
DismissedLight <1686188646@qq.com>
2022-01-08 19:29:40 +0800

    unlockfps in c#
    Co-Authored-By: Crsky <crskycode@hotmail.com>

---
+++ UnlockResult.cs
@@ -0,0 +1,19 @@
+﻿namespace DGP.Genshin.FPSUnlocking
+{
+    public enum UnlockResult
+    {
+        /// <summary>
+        /// 解锁成功，且游戏已经顺利运行完成
+        /// </summary>
+        Ok,
+        /// <summary>
+        /// 
+        /// </summary>
+        ProcessIsNull,
+        ProcessHasExited,
+        ModuleSearchTimeExceed,
+        ReadProcessMemoryFailed,
+        NoMatchedPatternFound,
+        UnlockerInvalid
+    }
+}
