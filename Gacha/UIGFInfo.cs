9652205e0d2081cd759a7853cfc5b23c351a3fe3
DismissedLight <1686188646@qq.com>
2022-02-06 20:29:53 +0800

    Update UIGFInfo.cs

--- Gacha/UIGFInfo.cs
+++ Gacha/UIGFInfo.cs
@@ -6,8 +6,8 @@ namespace DGP.Genshin.MiHoYoAPI.Gacha
     {
         [JsonProperty("uid")] public string? Uid { get; set; }
         [JsonProperty("lang")] public string? Language { get; set; }
-
         [JsonProperty("export_time")] public string? ExportTime { get; set; }
+        [JsonProperty("export_timestamp")] public long ExportTimeStamp { get; set; }
         [JsonProperty("export_app")] public string? ExportApp { get; set; }
         [JsonProperty("export_app_version")] public string? ExportAppVersion { get; set; }
         [JsonProperty("uigf_version")] public string? UIGFVersion { get; set; }
