91b87f2d4630d7af83a5b65b06ae6819277b1827
DismissedLight <1686188646@qq.com>
2022-03-13 16:09:13 +0800

    Update ConsumeItem.cs

--- Calculation/ConsumeItem.cs
+++ Calculation/ConsumeItem.cs
@@ -6,15 +6,28 @@ namespace DGP.Genshin.MiHoYoAPI.Calculation
     /// <summary>
     /// 要消耗的物品信息
     /// </summary>
-    public class ConsumeItem : Observable
+    public class ConsumeItem : Observable, ICloneable<ConsumeItem>
     {
         private bool isCompleted = false;
 
         [JsonProperty("id")] public int Id { get; set; }
         [JsonProperty("name")] public string? Name { get; set; }
         [JsonProperty("icon")] public string? Icon { get; set; }
-        [JsonProperty("num")] public int Num { get; set; }
+        [JsonProperty("num")] public double Num { get; set; }
         [JsonProperty("wiki_url")] public string? WikiUrl { get; set; }
         public bool IsCompleted { get => isCompleted; set => Set(ref isCompleted, value); }
+
+        public ConsumeItem Clone()
+        {
+            return new()
+            {
+                Id = Id,
+                Name = Name,
+                Icon = Icon,
+                Num = Num,
+                WikiUrl = WikiUrl,
+                IsCompleted = false
+            };
+        }
     }
 }
