4b219c56b6184ed31f79325b2d32a70444cbd029
DismissedLight <1686188646@qq.com>
2022-04-14 17:11:55 +0800

    Update GroupCalculable.cs

--- Calculation/GroupCalculable.cs
+++ Calculation/GroupCalculable.cs
@@ -4,14 +4,16 @@ namespace DGP.Genshin.MiHoYoAPI.Calculation
 {
     public abstract class GroupCalculable : Calculable
     {
-        [JsonProperty("group_id")] public int GroupId { get; set; }
+        [JsonProperty("group_id")]
+        public int GroupId { get; set; }
+
         public override PromotionDelta ToPromotionDelta()
         {
             return new()
             {
                 LevelCurrent = LevelCurrent,
                 LevelTarget = LevelTarget,
-                Id = GroupId
+                Id = GroupId,
             };
         }
     }
